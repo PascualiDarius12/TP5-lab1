b22af39a9eb6e44da2c83700c41c4679c6eeaacf
PascualiDarius12 <dario12bifa@gmail.com>
2023-04-29 18:34:12 -0300

    prueba de arreglo bidimensional irregular

--- src/tp5/laboratorio1/PruebaArreglo.java
+++ src/tp5/laboratorio1/PruebaArreglo.java
@@ -15,6 +15,26 @@ public class PruebaArreglo {
         }
         System.out.println("");
         Arreglo.sumarLista(unidimensional);
+        
+        //ARREGLO BIDIRECCIONAL IRREGULAR:
+        
+        
+        System.out.println("\n");
+        int arreglo[][] = new int[3][];
+
+        arreglo[0] = new int[3];
+        arreglo[1] = new int[5];
+        arreglo[2] = new int[2];
+
+        for (int i = 0; i < arreglo.length; i++) {
+            for (int j = 0; j < arreglo[i].length; j++) {
+                arreglo[i][j] = (int) (Math.random() * 100 + 1);
+                System.out.print("[" + arreglo[i][j] + "]");
+
+            }
+            System.out.println("");
+        }
+        System.out.println("Numero mayor:" + Arreglo.buscarMayor(arreglo));
     }
     
 }
